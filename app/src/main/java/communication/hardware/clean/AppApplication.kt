b43a86d518780b029384891d4a4d9c25c3a0a998
dani <daniel.avila@electronicid.eu>
2019-04-23 09:44:38 +0200

    remove unused log

--- app/src/main/java/communication/hardware/clean/AppApplication.kt
+++ app/src/main/java/communication/hardware/clean/AppApplication.kt
@@ -1,7 +1,6 @@
 package communication.hardware.clean
 
 import android.app.Application
-import android.util.Log
 import communication.hardware.clean.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -10,7 +9,6 @@ import org.koin.core.context.startKoin
 class AppApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Log.d("ccc", "onCreate App")
         startKoin {
             androidContext(this@AppApplication)
             androidLogger()
